81027d871c25691def761236d466e10e17928f67
mmm898 <phamnhatminh2000@gmail.com>
2023-10-31 20:57:09 +0700

    commit ordering system

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/input/message/listener/restaurantapproval/RestaurantApprovalResponseMessageListener.java
@@ -0,0 +1,9 @@
+package com.food.ordering.system.order.service.domain.ports.input.message.listener.restaurantapproval;
+
+import com.food.ordering.system.order.service.domain.dto.message.RestaurantApprovalResponse;
+import com.food.ordering.system.order.service.domain.entity.Restaurant;
+
+public interface RestaurantApprovalResponseMessageListener {
+    void orderApproved(RestaurantApprovalResponse restaurantApprovalResponse);
+    void orderRejected(RestaurantApprovalResponse restaurantApprovalResponse);
+}
